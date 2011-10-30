1a9ae23217251a3b159633385a317612ce1baf57
Daniël de Kok <me@danieldk.eu>
2011-10-30 18:39:56 +0100

    Move FFI bindings into a separate module.

--- lib/alpinocorpus.rb
+++ lib/alpinocorpus.rb
@@ -1,10 +1,10 @@
 require 'ffi'
 
-module EntriesIterate
+module EntriesIterate # :nodoc:
   def entriesIterate(iter)
     begin
       # Retrieve iterate value as a pointer, we have to free it.
-      strPtr = AlpinoCorpus::alpinocorpus_iter_value(iter)
+      strPtr = AlpinoCorpusFFI::alpinocorpus_iter_value(iter)
 
       # Convert and yield the string.
       str = strPtr.get_string(0)
@@ -12,34 +12,20 @@ module EntriesIterate
 
       # Free the C string.
       AlpinoCorpusLibC::free(strPtr)
-    end while AlpinoCorpus::alpinocorpus_iter_next(@reader, iter) != 0
+    end while AlpinoCorpusFFI::alpinocorpus_iter_next(@reader, iter) != 0
 
-    AlpinoCorpus::alpinocorpus_iter_destroy(iter)
+    AlpinoCorpusFFI::alpinocorpus_iter_destroy(iter)
   end
 end
 
 
 module AlpinoCorpus
-  extend FFI::Library
-
-  ffi_lib 'alpino_corpus'
-
-  attach_function :alpinocorpus_open, [:string], :pointer
-  attach_function :alpinocorpus_close, [:pointer], :void
-  attach_function :alpinocorpus_read, [:pointer, :string], :pointer
-  attach_function :alpinocorpus_entry_iter, [:pointer], :pointer
-  attach_function :alpinocorpus_query_iter, [:pointer, :string], :pointer
-  attach_function :alpinocorpus_iter_next, [:pointer, :pointer], :int
-  attach_function :alpinocorpus_iter_value, [:pointer], :pointer
-  attach_function :alpinocorpus_iter_destroy, [:pointer], :void
-  attach_function :alpinocorpus_is_valid_query, [:pointer, :string], :int
-
   class Reader
     include Enumerable
     include EntriesIterate
 
     def initialize(path)
-      @reader = AlpinoCorpus::alpinocorpus_open(path)
+      @reader = AlpinoCorpusFFI::alpinocorpus_open(path)
 
       if @reader.null?
         raise RuntimeError, "Could not open corpus."
@@ -49,7 +35,7 @@ module AlpinoCorpus
     end
 
     def each(&blk)
-      iter = AlpinoCorpus::alpinocorpus_entry_iter(@reader)
+      iter = AlpinoCorpusFFI::alpinocorpus_entry_iter(@reader)
       if iter.null?
         raise RuntimeError, "Could retrieve entries."
       end
@@ -60,7 +46,7 @@ module AlpinoCorpus
     end
 
     def read(name)
-      strPtr = AlpinoCorpus::alpinocorpus_read(@reader, name)
+      strPtr = AlpinoCorpusFFI::alpinocorpus_read(@reader, name)
       
       if strPtr.null?
         raise RuntimeError, "Could not read entry."
@@ -78,11 +64,11 @@ module AlpinoCorpus
     end
 
     def validQuery?(query)
-      AlpinoCorpus::alpinocorpus_is_valid_query(@reader, query) == 1
+      AlpinoCorpusFFI::alpinocorpus_is_valid_query(@reader, query) == 1
     end
 
     def self.finalize(reader)
-      proc { AlpinoCorpus::alpinocorpus_close(reader) }
+      proc { AlpinoCorpusFFI::alpinocorpus_close(reader) }
     end
   end
 
@@ -96,7 +82,7 @@ module AlpinoCorpus
     end
 
     def each(&blk)
-      iter = AlpinoCorpus::alpinocorpus_query_iter(@reader, @query)
+      iter = AlpinoCorpusFFI::alpinocorpus_query_iter(@reader, @query)
       if iter.null?
         raise RuntimeError, "Could not execute query."
       end
@@ -108,9 +94,23 @@ module AlpinoCorpus
   end
 end
 
-private
+module AlpinoCorpusFFI # :nodoc:
+  extend FFI::Library
 
-module AlpinoCorpusLibC
+  ffi_lib 'alpino_corpus'
+
+  attach_function :alpinocorpus_open, [:string], :pointer
+  attach_function :alpinocorpus_close, [:pointer], :void
+  attach_function :alpinocorpus_read, [:pointer, :string], :pointer
+  attach_function :alpinocorpus_entry_iter, [:pointer], :pointer
+  attach_function :alpinocorpus_query_iter, [:pointer, :string], :pointer
+  attach_function :alpinocorpus_iter_next, [:pointer, :pointer], :int
+  attach_function :alpinocorpus_iter_value, [:pointer], :pointer
+  attach_function :alpinocorpus_iter_destroy, [:pointer], :void
+  attach_function :alpinocorpus_is_valid_query, [:pointer, :string], :int
+end
+
+module AlpinoCorpusLibC # :nodoc:
     extend FFI::Library
     ffi_lib FFI::Library::LIBC
 
